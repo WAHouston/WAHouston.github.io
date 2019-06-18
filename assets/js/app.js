a572e2ba3c752ba52079bcbe394be76f625c38a5
Will Houston <wandrewhouston@gmail.com>
2019-06-18 13:22:38 -0400

    redirect attempt 1

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+if (window.location.href==="https://wahouston.github.io/") {
+    window.location.href = "http://www.wandrewhouston.com/"; 
+} else if (window.location.href==="https://wahouston.github.io/portfolio.html") {
+    window.location.href = "http://www.wandrewhouston.com/";
+} else if (window.location.href==="https://wahouston.github.io/index.html") {
+    window.location.href = "http://www.wandrewhouston.com/";
+} else if (window.location.href==="https://wahouston.github.io/contact.html") {
+    window.location.href = "http://www.wandrewhouston.com/";
+}
